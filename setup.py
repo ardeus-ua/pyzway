5d3b660704d365371d6a52854aab50797d3b56a3
Jakob Schlyter <jakob@kirei.se>
2017-04-09 13:24:53 +0200

    version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name='pyzway',
-    version='0.1',
+    version='0.1.0',
     description='Python for Z-Way',
     author='Jakob Schlyter',
     author_email='jakob@kirei.se',
