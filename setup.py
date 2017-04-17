e55e4b3688174cc4e0c1cc6122ed5950c0548260
Jakob Schlyter <jakob@kirei.se>
2017-04-17 22:21:26 +0200

    bump version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name='pyzway',
-    version='0.1.0',
+    version='0.2.0',
     description='Python for Z-Way',
     author='Jakob Schlyter',
     author_email='jakob@kirei.se',
